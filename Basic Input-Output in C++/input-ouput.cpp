0fcd0b35812c0a0aacca5629b1c1e5f8e44cb81a
meetshah1205 <meetshah9625@gmail.com>
2024-11-28 19:26:56 +0530

    Basic I/O in C++

---
+++ Basic Input-Output in C++/input-ouput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	cout<< /* '<<' is insertion operater */ "Enter the value of num1:\n";
+	cin>> /* '>>' is extraction operation */ num1;
+	
+	cout<< /* '<<' is insertion operater */ "Enter the value of num2:\n";
+	cin>> /* '>>' is extraction operation */ num2;
+
+	cout<<"The sum of the numbers is " << num1 + num2;
+	return 0;
+}
