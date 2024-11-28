8a040266eef0d19750b3541eb19a1dfe911efe67
meetshah1205 <meetshah9625@gmail.com>
2024-11-28 18:59:28 +0530

    Variables and comments in C++

---
+++ Variables and Comments/variables-and-comments.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// This is a single line comment in C++
+/*
+This
+is
+a
+multi -
+line
+comment
+in
+C++
+*/
+int main(){
+	int sum = 6; // Create an integer variable sum and store 6 into it
+	cout << sum;
+	return 0;
+}
